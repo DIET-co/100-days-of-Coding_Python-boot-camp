f3d0bc39540e3b8ddcdf521911e892a86bee254e
likesh k <k.likesh27@gmail.com>
2024-06-14 22:34:13 +0530

    Update Random.py

--- DAY-06/Random.py
+++ DAY-06/Random.py
@@ -1,13 +1,5 @@
 def is_prime(n: int) -> bool:
-    """
-    Determine if a given number is prime.
     
-    Arguments:
-    n (int): The number to check for primality.
-    
-    Returns:
-    bool: True if the number is prime, False otherwise.
-    """
     if n < 2:
         return False
     for i in range(2, int(n ** 0.5) + 1):
